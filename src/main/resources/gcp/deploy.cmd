b30197bc9c5110d86eeca512fed47a362c353e50
Uanderson Soares <x@usoar.es>
2019-04-23 16:26:27 +0200

    Add CMD version of deploy file

--- src/main/resources/gcp/deploy.cmd
+++ src/main/resources/gcp/deploy.cmd
@@ -1,1 +1,46 @@
-gcloud builds submit --config cloudbuild.yaml .
+@ECHO off
+@SETLOCAL
+SET CREATE_PROJECT=${createProject}
+SET PROJECT_ID=${projectId}
+
+WHERE /Q gcloud
+IF %ERRORLEVEL% NEQ 0 (
+    GOTO printInstructions
+) else (
+    GOTO deploy
+)
+
+:createProject
+CALL gcloud projects create %PROJECT_ID%
+CALL gcloud config set project %PROJECT_ID%
+CALL gcloud config set run/region us-central1
+CALL gcloud beta billing accounts list
+
+@REM It reads the account ID
+SET /P ACCOUNT_ID="Enter the billing account ID you want to use: "
+
+CALL gcloud beta billing projects link %PROJECT_ID% --billing-account=%ACCOUNT_ID%
+CALL gcloud services enable cloudbuild.googleapis.com
+CALL gcloud services enable run.googleapis.com
+
+FOR /F "tokens=*" %%a in ('gcloud beta projects describe %PROJECT_ID% --format="get(projectNumber)"') DO SET PROJECT_NUMBER=%%a
+CALL gcloud projects add-iam-policy-binding %PROJECT_ID% --member serviceAccount:%PROJECT_NUMBER%@cloudbuild.gserviceaccount.com --role roles/run.admin
+CALL gcloud projects add-iam-policy-binding %PROJECT_ID% --member serviceAccount:%PROJECT_NUMBER%@cloudbuild.gserviceaccount.com --role roles/iam.serviceAccountUser
+CALL gcloud builds submit --config cloudbuild.yaml .
+
+:deploy
+IF "%CREATE_PROJECT%" == "true" (
+    GOTO createProject
+) ELSE (
+    CALL gcloud config set project %PROJECT_ID%
+    CALL gcloud builds submit --config cloudbuild.yaml .
+)
+
+:printInstructions
+ECHO ========================================================================
+ECHO In order to deploy this project, you need gcloud set in your PATH.
+ECHO You can download it from https://cloud.google.com/sdk
+ECHO ========================================================================
+
+@ENDLOCAL
+EXIT /B 0
